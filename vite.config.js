609c27cd55436bafdc2e32bb2397f97f28c7386d
VandalxGod <sumit.tutu2506@gmail.com>
2026-01-19 12:18:31 +0530

    vite.config update remove url

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/gym-management/",   // 🔥 REQUIRED for sub-path hosting
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
