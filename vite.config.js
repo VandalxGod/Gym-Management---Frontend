1002ad4acfce2c1f91b0e7ab03b24dec51ce0cf3
VandalxGod <sumit.tutu2506@gmail.com>
2026-01-19 12:41:44 +0530

    fixing to the normal url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // base: "/gym-management/",   // 🔥 REQUIRED for sub-path hosting
+  plugins: [react(), tailwindcss()],
 });
